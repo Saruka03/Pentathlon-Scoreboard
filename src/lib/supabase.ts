6674f2fef4704a7221d81e592cf22f29c4e40142
Saruka Umainesan <umainesansaruka@gmail.com>
2026-01-22 22:07:48 +0700

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://qbsblqdpcsinsdvogbrb.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFic2JscWRwY3NpbnNkdm9nYnJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5MDM1MjgsImV4cCI6MjA4NDQ3OTUyOH0.7cTd3yEBgsUlGSsAjF5KUvVQOg16tItZ7_QNLN4Jytk";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
